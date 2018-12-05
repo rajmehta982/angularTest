e13eecaa1ed584628c18556d6437dd2aea59abaf
Raj Mehta <raj@coursera.com>
2018-12-06 00:26:07 +0530

    Assignment 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit,Input } from '@angular/core';
-
-
+import { Dish } from '../shared/dish';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -9,12 +11,20 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
-  dish;
 
-  constructor() { }
+  dish:Dish;
+
+  constructor(private dishService: DishService,
+     private location:Location,
+   private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void{
+    this.location.back();
   }
 
 }
---
+++ src/app/shared/leader.ts
@@ -0,0 +1,8 @@
+export class Leader{
+  id : string;
+  name : string;
+  image : string;
+  abbr : string;
+  featured : boolean;
+  description : string;
+}
